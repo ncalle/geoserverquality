4d04bd4f0c63e33a60a0eccbe1ebb2b61943b979
DESKTOP-87QNMFB\nati <nacalle@gmail.com>
2017-02-07 20:29:26 -0300

    Idem anterior para excepciones de configuracion

--- EJBs/ejbModule/daos/DAOConfigurationException.java
+++ EJBs/ejbModule/daos/DAOConfigurationException.java
@@ -1,9 +1,12 @@
 package daos;
 
+import javax.ejb.ApplicationException;
+
 /**
  * Representa una excepcion en la configuración DAO, que no puede ser resuelta en tiempo de ejecucion,
  * como un archivo faltante en el classpath o una propiedad faltante.
  */
+@ApplicationException(rollback = true)
 public class DAOConfigurationException extends RuntimeException {
 
     private static final long serialVersionUID = 1L;
