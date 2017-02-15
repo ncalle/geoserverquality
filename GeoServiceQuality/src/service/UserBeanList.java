7469a93a0caabcba3ac4d968266251afbbdf57a5
ramiro30982 <ramiro30982@gmail.com>
2017-02-14 23:09:13 -0300

    Se solucionan varios bugs de edit mode y seleccion a la vez. Se agregan notificaciones de cuando se actualiza o elimina un usuario.

--- GeoServiceQuality/src/service/UserBeanList.java
+++ GeoServiceQuality/src/service/UserBeanList.java
@@ -4,9 +4,15 @@ import java.util.List;
 
 import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.RequestScoped;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
 
+import org.primefaces.event.RowEditEvent;
+import org.primefaces.event.SelectEvent;
+
+import entity.MeasurableObject;
 import entity.User;
 import dao.DAOException;
 import dao.UserBean;
@@ -14,12 +20,12 @@ import dao.UserBeanRemote;
 
 
 @ManagedBean(name="userBeanList")
-@RequestScoped
+@SessionScoped 
 public class UserBeanList {
 	    
-	private List<User> listUsers;
-	
+	private List<User> listUsers;	
 	private User selectedUser;
+	private MeasurableObject listUserMeasurableObjects;
 	
 	@EJB
     private UserBeanRemote uDao = new UserBean();
@@ -47,20 +53,36 @@ public class UserBeanList {
     public User getSelectedUser() {
         return selectedUser;
     }
- 
-    public void setSelectedUser(User selectedUser) {
-    	System.out.println("Usuario a actualizar: "+ selectedUser);
-    	
-    	this.selectedUser = selectedUser;
-    	uDao.update(selectedUser);
-    }
+     
+	public MeasurableObject getListUserMeasurableObjects() {
+		return listUserMeasurableObjects;
+	}
+
+	public void setListUserMeasurableObjects(MeasurableObject listUserMeasurableObjects) {
+		this.listUserMeasurableObjects = listUserMeasurableObjects;
+	}      
     
-    public void deleteUser() {
-    	
-    	System.out.println("Usuario a borrar: "+ selectedUser);
-    	
+    public void setSelectedUser(User selectedUser) {   	
+    	this.selectedUser = selectedUser;
+    }
+	
+	public void deleteUser() {   	
     	uDao.delete(selectedUser);    	
     	listUsers.remove(selectedUser);
+        selectedUser = null;
+        
+		FacesMessage msg = new FacesMessage("Usuario eliminado correctamente.");       
+        FacesContext.getCurrentInstance().addMessage(null, msg);
+    }
+    
+    public void onRowSelect(SelectEvent event) {
+        // actualizar objetos medibles del usuario
+    }
+       	
+	public void onRowEdit(RowEditEvent event) {    	
+		uDao.update(((User) event.getObject()));
+		FacesMessage msg = new FacesMessage("Usuario editado correctamente.");       
+        FacesContext.getCurrentInstance().addMessage(null, msg);
         selectedUser = null;
     }
 
