7ef547c4d0f17c388e1d2551298b27256d699bd8
ramiro30982 <ramiro30982@gmail.com>
2017-03-03 12:23:28 -0300

    Cambios en pagina de Profile. Listado y selección de perfil.

--- GeoServiceQuality/src/service/ProfileBeanList.java
+++ GeoServiceQuality/src/service/ProfileBeanList.java
@@ -4,8 +4,13 @@ import java.util.List;
 
 import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.RequestScoped;
+import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
+
+import org.primefaces.event.RowEditEvent;
+import org.primefaces.event.SelectEvent;
 
 import entity.Profile;
 import dao.DAOException;
@@ -14,34 +19,61 @@ import dao.ProfileBeanRemote;
 
 
 @ManagedBean(name="profileBeanList")
-@RequestScoped
+@ViewScoped
 public class ProfileBeanList {
 	
-    
-	private List<Profile> list;
+	private List<Profile> listProfile;
+	private Profile selectedProfile;
 	
 	@EJB
-    private ProfileBeanRemote profileDao = new ProfileBean();
+    private ProfileBeanRemote pDao = new ProfileBean();
 	
 	
 	@PostConstruct
 	private void init()	{
-		try {
-			
-            list = profileDao.list();
-            System.out.println("list size: "+ list.size());
-	            
+		try {		
+			listProfile = pDao.list();	            
     	} catch(DAOException e) {
     		e.printStackTrace();
     	} 
 	}
 	
-	public List<Profile> getList() {
-		return list;
+	public List<Profile> getListProfile() {
+		return listProfile;
 	}
 	
-	public void setList(List<Profile> list) {
-		this.list = list;
+	public void setListProfile(List<Profile> listProfile) {
+		this.listProfile = listProfile;
 	}
+
+	public Profile getSelectedProfile() {
+		return selectedProfile;
+	}
+
+	public void setSelectedProfile(Profile selectedProfile) {
+		System.out.println("selectedProfile: " + selectedProfile);
+		this.selectedProfile = selectedProfile;
+	}
+	
+	public void deleteProfile() {   	
+    	pDao.delete(selectedProfile);    	
+    	listProfile = pDao.list();
+    	selectedProfile = null;
+        
+		FacesMessage msg = new FacesMessage("Perfil eliminado correctamente.");       
+        FacesContext.getCurrentInstance().addMessage(null, msg);
+    }
+    
+    public void onRowSelect(SelectEvent event) {
+    	//agregar codigo de ser necesario
+    }
+       	
+	public void onRowEdit(RowEditEvent event) {    	
+		Profile p = ((Profile) event.getObject());
+			
+		//pDao.update(p);
+		FacesMessage msg = new FacesMessage("Perfil editado correctamente.");
+        FacesContext.getCurrentInstance().addMessage(null, msg);
+    }
 
 }
